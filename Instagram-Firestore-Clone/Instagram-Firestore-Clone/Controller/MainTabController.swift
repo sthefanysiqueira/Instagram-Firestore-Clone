356374d53440e6320d3f22b7058495b68f1a9fba
sthefanysiqueira <sthefanycfds@gmail.com>
2024-06-25 14:44:38 -0300

    initial commit

---
+++ Instagram-Firestore-Clone/Instagram-Firestore-Clone/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  Instagram-Firestore-Clone
+//
+//  Created by Sthefany Caroline Figuerêdo de Siqueira on 25/06/24.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red // Define a cor de fundo como vermelha
+    }
+    
+}
