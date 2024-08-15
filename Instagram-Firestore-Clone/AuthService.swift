d2fa9a4e44daeccf363a38c63acbab13908e7b55
sthefanysiqueira <sthefanycfds@gmail.com>
2024-08-15 14:03:07 -0300

    chore: Register user in Firebase

---
+++ Instagram-Firestore-Clone/AuthService.swift
@@ -0,0 +1,43 @@
+//
+//  AuthService.swift
+//  Instagram-Firestore-Clone
+//
+//  Created by Sthefany Caroline Figuerêdo de Siqueira on 11/08/24.
+//
+
+import UIKit
+import Firebase
+import FirebaseFirestoreInternal
+
+struct AuthCredentials {
+    let email: String
+    let password: String
+    let fullname: String
+    let username: String
+    let profileImage: UIImage
+}
+
+struct AuthService {
+    static func registerUser(withCredential credentials: AuthCredentials, completion: @escaping(Error?) -> Void) {
+        
+        ImageUploader.uploadImage(image: credentials.profileImage) { imageURL in
+            Auth.auth().createUser(withEmail: credentials.email, password: credentials.password) { (result, error) in
+                
+                if let error = error {
+                    print("DEBUG: Failed to register user \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let uid = result?.user.uid else { return }
+                
+                let data: [String: Any] = ["email": credentials.email,
+                                           "fullname": credentials.fullname,
+                                           "profileImageUrl": imageURL,
+                                           "uid": uid,
+                                           "username": credentials.username]
+                
+                Firestore.firestore().collection("users").document(uid).setData(data, completion: completion)
+            }
+        }
+    }
+}
--- Instagram-Firestore-Clone/Instagram-Firestore-Clone/Controller/Authentication/RegistrationController.swift
+++ Instagram-Firestore-Clone/Instagram-Firestore-Clone/Controller/Authentication/RegistrationController.swift
@@ -12,6 +12,7 @@ class RegistrationController: UIViewController {
     // MARK: - Properties
     
     private var viewModel = RegistrationViewModel()
+    private var profileImage: UIImage?
     
     private let plushPhotoButton: UIButton = {
         let button = UIButton(type: .system)
@@ -44,6 +45,11 @@ class RegistrationController: UIViewController {
         button.layer.cornerRadius = 5
         button.setHeight(50)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+<<<<<<< Updated upstream
+=======
+        button.isEnabled = false
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+>>>>>>> Stashed changes
         return button
     }()
     
@@ -63,6 +69,27 @@ class RegistrationController: UIViewController {
     }
     
     // MARK: - Actions
+    
+    @objc func handleSignUp() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        guard let fullname = fullnameTextField.text else { return }
+        guard let username = usernameTextField.text?.lowercased() else { return }
+        guard let profileImage = self.profileImage else { return }
+        
+        let credentials = AuthCredentials(email: email, password: password,
+                                          fullname: fullname, username: username, 
+                                          profileImage: profileImage)
+
+        AuthService.registerUser(withCredential: credentials) { error in
+            if let error = error {
+                print("DEBUG: Failed to register user \(error.localizedDescription)")
+                return
+            }
+            
+            print("DEBUG: Successfully registered user with firestore..")
+        }
+    }
     
     @objc func handleShowLogin() {
         navigationController?.popViewController(animated: true) // Animação de arrastar de volta pra página de Login
@@ -139,6 +166,7 @@ extension RegistrationController: UIImagePickerControllerDelegate, UINavigationC
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         guard let selectedImage = info[.editedImage] as? UIImage else { return }
+        profileImage = selectedImage
         
         plushPhotoButton.layer.cornerRadius = plushPhotoButton.frame.width / 2
         plushPhotoButton.layer.masksToBounds = true
