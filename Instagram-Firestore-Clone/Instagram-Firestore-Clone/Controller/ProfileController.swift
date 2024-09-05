6ed8aab6deba3bf9bd9e9328eef143c95112e2bb
sthefanysiqueira <sthefanycfds@gmail.com>
2024-09-05 08:46:06 -0300

    chore: Update user after authentication

--- Instagram-Firestore-Clone/Instagram-Firestore-Clone/Controller/ProfileController.swift
+++ Instagram-Firestore-Clone/Instagram-Firestore-Clone/Controller/ProfileController.swift
@@ -14,30 +14,30 @@ class ProfileController: UICollectionViewController {
     
     // MARK: - Properties
     
-    var user: User? {
-        didSet { collectionView.reloadData() }
-    }
+    private var user: User
     
     // MARK: - Lifecycle
+    
+    init(user: User) {
+        self.user = user
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
-        fetchUser()
     }
     
     // MARK: - API
     
-    func fetchUser() {
-        UserService.fetchUser { user in
-            self.user = user
-            self.navigationItem.title = user.username
-        }
-    }
-    
     // MARK: - Helpers
     
     func configureCollectionView() {
+        navigationItem.title = user.username
         collectionView.backgroundColor = .white
         collectionView.register(ProfileCell.self, forCellWithReuseIdentifier: cellIdentifier)
         collectionView.register(ProfileHeader.self, 
@@ -62,9 +62,7 @@ extension ProfileController {
         
         let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath) as! ProfileHeader
         
-        if let user = user {
             header.viewModel = ProfileHeaderViewModel(user: user)
-        }
         
         return header
     }
