fc9bfcda80653ff9f519b2fe1a35b4af6433cc6f
sthefanysiqueira <sthefanycfds@gmail.com>
2024-09-10 10:29:08 -0300

    feat: Fetching and displaying users

--- Instagram-Firestore-Clone/Instagram-Firestore-Clone/Controller/SearchController.swift
+++ Instagram-Firestore-Clone/Instagram-Firestore-Clone/Controller/SearchController.swift
@@ -13,12 +13,24 @@ class SearchController: UITableViewController {
     
     // MARK: - Properties
     
+    private var users = [User]()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureTableView()
+        fetchUsers()
+    }
+    
+    // MARK: - API
+    
+    func fetchUsers() {
+        UserService.fetchUsers { users in
+            self.users = users
+            self.tableView.reloadData()
+        }
     }
     
     // MARK: - Helpers
@@ -37,12 +49,13 @@ extension SearchController {
     
     // define o número de linhas que a tableView terá
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return users.count
     }
     
     // define oque cada célula ou linha irá conter
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
+        cell.user = users[indexPath.row]
         return cell
     }
 }
