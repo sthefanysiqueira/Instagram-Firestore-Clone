794589eda5cf78614c7fddfb03debe3db44a052c
sthefanysiqueira <sthefanycfds@gmail.com>
2024-10-29 14:10:55 -0300

    chore: Unfollowing user & updating UI

--- Instagram-Firestore-Clone/Instagram-Firestore-Clone/API/UserService.swift
+++ Instagram-Firestore-Clone/Instagram-Firestore-Clone/API/UserService.swift
@@ -36,6 +36,11 @@ struct UserService {
     }
     
     static func unfollow(uid: String, completion: @escaping(FirestoreCompletion)) {
-        
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+
+        COLLECTION_FOLLOWING.document(currentUid).collection("user-following").document(uid).delete { error in
+            
+            COLLECTION_FOLLOWERS.document(uid).collection("user-followers").document(currentUid).delete(completion: completion)
+        }
     }
 }
--- Instagram-Firestore-Clone/Instagram-Firestore-Clone/Controller/ProfileController.swift
+++ Instagram-Firestore-Clone/Instagram-Firestore-Clone/Controller/ProfileController.swift
@@ -104,10 +104,14 @@ extension ProfileController: ProfileHeaderDelegate {
         if user.isCurrentUser {
             print("DEBUG: Show edit profile here..")
         } else if user.isFollowed {
-            print("DEBUG: Handle unfollow user here..")
+            UserService.unfollow(uid: user.uid) { error in
+                self.user.isFollowed = false
+                self.collectionView.reloadData()
+            }
         } else {
             UserService.follow(uid: user.uid) { error in
-                print("DEBUG: Did follow user. Update UI now..")
+                self.user.isFollowed = true
+                self.collectionView.reloadData()
             }
         }
     }
